50fd04c62585ac2931a54ab7d2af44a246cde3e5
Ben <simons.ben0@gmail.com>
2017-01-16 22:00:53 -0500

    Moved GPS refresh check to main loop to clear buffer properly

--- GPSTest/GPSTest.ino
+++ GPSTest/GPSTest.ino
@@ -1,5 +1,6 @@
 #include <EEPROM.h>
 #include "Adafruit_GPS.h"
+#include<SoftwareSerial.h>
 
 //Define Global constants and variables----------------------------------------
 #define saveSwitch 3
@@ -7,7 +8,7 @@
 
 // GPS constants
 #define MAXLINELENGTH 120
-#define GPS_SERIAL Serial
+#define newSerial Serial
 #define GPS_BAUD 9600
 Adafruit_GPS GPS;
 
@@ -19,6 +20,8 @@ Adafruit_GPS GPS;
   #define DEBUG_PRINT(x)
   #define DEBUG_PRINTLN(x)
 #endif
+
+//SoftwareSerial newSerial(5, 6);
 
 //gps variables and functions
 char nmeaBuf[MAXLINELENGTH];
@@ -34,9 +37,8 @@ void setup() {
   int i;
   Serial.begin(9600);
   pinMode(LEDPin, OUTPUT); //Assign pin mode for LED
-
-  //Initialize button using interupt pin
   pinMode(saveSwitch, INPUT_PULLUP);
+
   setupGPS();
 }
 
@@ -60,7 +62,7 @@ void loop() {
         Serial.println("Cleared.");
         clearmem();
       }
-      if(tme > 2000) {
+      else if(tme > 2000) {
         Serial.println("Long");
         longpress();
       }
@@ -70,6 +72,7 @@ void loop() {
       }
     }
   }
+  getGPSLocation();
 
   delay(50);
 }
@@ -78,7 +81,7 @@ void loop() {
 //Flash the current number of saved data points
 void longpress()  {
   int i, count; //Declare counter and read number of data entries
-  delay(250); //Allow time to look at LED after button press
+  delay(750); //Allow time to look at LED after button press
   count = EEPROM.read(1);
   
   for(i=0;i<count;i++)  {
@@ -96,17 +99,18 @@ void longpress()  {
 //Confirmation flash to acknowledge save of location
 void confirm() { 
   int i;
+  delay(300);
   for(i=0;i<2;i++) {
     digitalWrite(LEDPin, HIGH);
     delay(250);
     digitalWrite(LEDPin, LOW);
-    delay(400);
+    delay(150);
   }
 }
 
 //Update GPS location------------------------------------------------------------
 void updateloc() {
-  getGPSLocation();
+  //getGPSLocation();
   lng = GPS.longitude;
   lat = GPS.latitude;
   Serial.println("Update location");
@@ -117,6 +121,8 @@ void updateloc() {
   Serial.println(")");
   Serial.print("GPS.time = ");
   Serial.println(GPS.milliseconds);
+  lng = 123.456;
+  lat = 654.321;
 }
 
 //Save GPS Function--------------------------------------------------------------
@@ -126,7 +132,6 @@ void saveData() {
   unsigned char* pt = (unsigned char*) &lng;
   int i, len;
   len = EEPROM.read(1);
-  
   
   for(i=0;i<4;i++)
     EEPROM.write(i + len*4 + 2, pt[i]);
@@ -156,31 +161,35 @@ void setupGPS()  {
   GPS.init();
 
   // Start the serial communication
-  GPS_SERIAL.begin(GPS_BAUD);
+  newSerial.begin(GPS_BAUD);
 
   // Commands to configure GPS:
-  GPS_SERIAL.println(PMTK_SET_NMEA_OUTPUT_RMCONLY);     // Set to only output GPRMC (has all the info we need),
-  GPS_SERIAL.println(SET_NMEA_UPDATE_RATE_5HZ);     // Increase rate strings sent over serial
-  GPS_SERIAL.println(PMTK_API_SET_FIX_CTL_5HZ);     // Increase rate GPS 'connects' and syncs with satellites
-  GPS_SERIAL.println(ENABLE_SBAB_SATELLITES);       // Enable using a more accurate type of satellite
-  GPS_SERIAL.println(ENABLE_USING_WAAS_WITH_SBSB_SATS);   // Enable the above satellites in 'fix' mode (I think)
+  newSerial.println(PMTK_SET_NMEA_OUTPUT_RMCONLY);     // Set to only output GPRMC (has all the info we need),
+  newSerial.println(SET_NMEA_UPDATE_RATE_5HZ);     // Increase rate strings sent over serial
+  newSerial.println(PMTK_API_SET_FIX_CTL_5HZ);     // Increase rate GPS 'connects' and syncs with satellites
+  newSerial.println(ENABLE_SBAB_SATELLITES);       // Enable using a more accurate type of satellite
+  newSerial.println(ENABLE_USING_WAAS_WITH_SBSB_SATS);   // Enable the above satellites in 'fix' mode (I think)
   delay(3000);  //Not really sure if needed.
 
   // Flush the GPS input (still unsure if the GPS sends response to the above commands)
-  while (GPS_SERIAL.available()) {
-    //GPS_SERIAL.read();
+  while (newSerial.available()) {
+    //newSerial.read();
     DEBUG_PRINT("FLUSH RESPONSE: ");
-    DEBUG_PRINT(GPS_SERIAL.read());
+    DEBUG_PRINT(newSerial.read());
     DEBUG_PRINTLN("");
   }
 
 }
 
 void getGPSLocation() {
-  while (GPS_SERIAL.available()) {
 
-    nmeaBuf[nmeaBufInd] = GPS_SERIAL.read();
-    //DEBUG_PRINT(nmeaBuf[nmeaBufInd]);
+  //DEBUG_PRINT("Bytes available: ");
+  //DEBUG_PRINTLN(newSerial.read());
+  while (newSerial.available()) {
+
+    nmeaBuf[nmeaBufInd] = newSerial.read();
+    DEBUG_PRINT(nmeaBuf[nmeaBufInd]);
+    
     if (nmeaBuf[nmeaBufInd++] == '\n') { // Increment index after checking if current character signifies the end of a string
       nmeaBuf[nmeaBufInd - 1] = '\0'; // Add null terminating character (note: -1 is because nmeaBufInd is incremented in if statement)
       newParsedData = GPS.parse(nmeaBuf);   // This parses the string, and updates the values of GPS.lattitude, GPS.longitude etc.
